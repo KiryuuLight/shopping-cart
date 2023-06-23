10d6b6bc37d4673b8b297eb40309c2693d6fa209
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2023-06-23 17:37:16 -0500

    Add types file to handle reducer function

---
+++ src/context/types.js
@@ -0,0 +1,4 @@
+export const ADD_ITEM = 'ADD_ITEM';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const INCREASE_QUANTITY = 'INCREASE_QUANTITY';
+export const DECREASE_QUANTITY = 'DECREASE_QUANTITY';
