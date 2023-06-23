da863ef471eefd8c9bd858e21ee2b931b345abe8
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2023-06-23 17:36:49 -0500

    Add Component

---
+++ src/context/Cart/CartState.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useReducer } from 'react';
+import { reduceFn } from './CartReducer';
+import { CartContext } from './CartContext';
+import {
+  ADD_ITEM,
+  REMOVE_ITEM,
+  INCREASE_QUANTITY,
+  DECREASE_QUANTITY,
+} from '../types';
+
+const CartState = (props) => {
+  const initialState = {
+    cart: [],
+  };
+
+  const [state, dispatch] = useReducer(reduceFn, initialState);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
+  const addItem = (product) => {
+    dispatch({ type: ADD_ITEM, payload: product });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: id });
+  };
+
+  const increaseItemQuantity = (id) => {
+    dispatch({ type: INCREASE_QUANTITY, payload: id });
+  };
+
+  const decreaseItemQuantity = (id) => {
+    dispatch({ type: DECREASE_QUANTITY, payload: id });
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart: state.cart,
+        addItem,
+        removeItem,
+        increaseItemQuantity,
+        decreaseItemQuantity,
+      }}
+    >
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartState;
