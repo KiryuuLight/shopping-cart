2f9b8821eb04bc9115a7658ecbce750ea08461df
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2023-08-01 20:26:52 -0500

    Remove old files

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export const getProducts = async () => {
-  const data = await fetch('https://fakestoreapi.com/products');
-  return data;
-};
-
-export const getProduct = async (id) => {
-  const data = await fetch(`https://fakestoreapi.com/products/${id}`);
-  return data;
-};
